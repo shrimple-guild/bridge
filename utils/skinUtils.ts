abe3e7ec7dda20eb420597dac319b842d2d050fe
Appability <appable@icloud.com>
2022-11-28 00:04:04 -0800

    skins but betterer

--- utils/skinUtils.ts
+++ utils/skinUtils.ts
@@ -1,22 +1,48 @@
+import Database from "better-sqlite3"
 import fetch from "node-fetch"
 import sharp from "sharp"
 
-export const steve = "LyANKx4NLx8PKBwLJBgIJhoKKx4NKh0NKx4NKx4NKx4NMyQRQioSPyoVLB4OKBwLKx4NtolsvY5yxpaAvYtyvY50rHZaNCUSqn1mtIRtqn1mrYBtnHJcu4lynGlMnGlMtIRt////Uj2JtXtnu4lyUj2J////qn1mnGNGs3tit4JyakAwakAwvohsompHgFM0kF5Dll9Ad0I1d0I1d0I1d0I1j14+gVM5b0UsbUMqgVM5gVM5ek4zg1U7g1U7ek4z"
+const db = new Database("./data/members.db")
+const steve = "LyANKx4NLx8PKBwLJBgIJhoKKx4NKh0NKx4NKx4NKx4NMyQRQioSPyoVLB4OKBwLKx4NtolsvY5yxpaAvYtyvY50rHZaNCUSqn1mtIRtqn1mrYBtnHJcu4lynGlMnGlMtIRt////Uj2JtXtnu4lyUj2J////qn1mnGNGs3tit4JyakAwakAwvohsompHgFM0kF5Dll9Ad0I1d0I1d0I1d0I1j14+gVM5b0UsbUMqgVM5gVM5ek4zg1U7g1U7ek4z"
+const timeout = 60
 
-async function getMinecraftUuid(username: string): Promise<string | undefined> {
-  try {
-    const uuidResponse = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
-    if (uuidResponse.status != 200) throw new Error(`Mojang API returned code ${uuidResponse.status} while getting the UUID of ${username}.`)
-    return (await uuidResponse.json() as any).id
-  } catch (e) {
-    console.warn(e)
-  }
+db.prepare(`
+CREATE TABLE IF NOT EXISTS Members (
+  username TEXT PRIMARY KEY,
+  skin TEXT NOT NULL,
+  lastUpdated INTEGER NOT NULL
+)
+`).run()
+
+const skinSelect = db.prepare(`
+SELECT skin, lastUpdated
+FROM Members
+WHERE username = ?
+`)
+
+const insertSkin = db.prepare(`
+INSERT OR REPLACE INTO Members 
+VALUES (:username, :skin, :lastUpdated)
+`)
+
+async function updateSkin(username: string): Promise<string | undefined> {
+  const skin = await getMinecraftAvatar(username)
+  if (!skin) return
+  insertSkin.run({ username: username, skin: skin, lastUpdated: Date.now() })
+  return skin
 }
 
-async function getMinecraftAvatarFromUuid(uuid: string): Promise<string | undefined> {
+async function getMinecraftAvatar(username: string): Promise<string | undefined> {
   try {
+    const uuidResponse = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+    if (uuidResponse.status != 200) {
+      throw new Error(`Mojang API returned code ${uuidResponse.status} while getting the UUID of ${username}.`)
+    }
+    const uuid = (await uuidResponse.json() as any).id
     const profileResponse = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
-    if (profileResponse.status !== 200) throw new Error(`Mojang API returned code ${profileResponse.status} while getting profile data for ${uuid}.`)
+    if (profileResponse.status !== 200) {
+      throw new Error(`Mojang API returned code ${profileResponse.status} while getting profile data for ${uuid}.`)
+    }
     const content = await profileResponse.json() as any
     const textureUrl = JSON.parse(Buffer.from(content.properties[0].value, "base64").toString("ascii")).textures.SKIN.url
     const textureResponse = await fetch(textureUrl)
@@ -34,13 +60,7 @@ async function getMinecraftAvatarFromUuid(uuid: string): Promise<string | undefi
   }
 } 
 
-export async function getMinecraftAvatar(username: string): Promise<string | undefined> {
-  const uuid = await getMinecraftUuid(username)
-  if (!uuid) return
-  return await getMinecraftAvatarFromUuid(uuid)
-}
-
-export async function getSkinPng(data: string): Promise<Buffer> {
+async function getSkinPng(data: string): Promise<Buffer> {
   const array = new Uint8ClampedArray(Buffer.from(data, "base64"))
   return await sharp(array, {raw: {width: 8, height: 8, channels: 3}})
     .resize(128, 128, { kernel: sharp.kernel.nearest })
@@ -48,3 +68,12 @@ export async function getSkinPng(data: string): Promise<Buffer> {
     .toBuffer()
 }
 
+export async function getSkin(username: string): Promise<Buffer> {
+  let { skin, lastUpdated } = skinSelect.get(username) ?? {}
+  if (!skin) {
+    skin = await updateSkin(username)
+  } else if ((Date.now() - lastUpdated) > timeout) {
+    setImmediate(() => updateSkin(username))
+  }
+  return getSkinPng(skin ?? steve)
+}
