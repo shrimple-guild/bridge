148c173bd1fde045d7cbc6d4a4196f0cd558723a
Appability <appable@icloud.com>
2023-02-07 15:49:42 -0800

    add WIP api wrapper
    consider it future proofing for hypixel api authentication :100:

---
+++ api/HypixelAPI.ts
@@ -0,0 +1,46 @@
+import { fetchWithTimeout } from "../utils/apiUtils.js"
+import { HypixelGuildMember } from "./HypixelGuildMember.js"
+import { HypixelPlayer } from "./HypixelPlayer.js"
+import { SkyblockProfiles } from "./SkyblockProfiles.js"
+
+export class HypixelAPI {
+  private apiKey: string
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey
+  }
+
+  async fetchPlayer(uuid: string): Promise<HypixelPlayer> {
+    const response = await fetchHypixel("/player", { uuid: uuid, key: this.apiKey}) as any
+    if (response.player == null) throw new Error(`This player has not joined Hypixel!`)
+    return new HypixelPlayer(response.player)
+  }
+  
+  async fetchProfiles(uuid: string): Promise<SkyblockProfiles> {
+    const uuidTrimmed = uuid.replaceAll("-", "")
+    const response = await fetchHypixel("/skyblock/profiles", { uuid: uuidTrimmed, key: this.apiKey}) as any
+    if (response.profiles == null || response.profiles.length == 0) {
+      throw new Error(`This player has not joined SkyBlock!`)
+    }
+    return new SkyblockProfiles(response.profiles, uuidTrimmed)
+  }
+  
+  async fetchGuildMembers(guildId: string): Promise<HypixelGuildMember[]> {
+    const response = await fetchHypixel("/guild", { id: guildId, key: this.apiKey}) as any
+    if (!response.guild) throw new Error(`This guild does not exist!`)
+    const members = response.guild.members as any[]
+    return members.map(member => new HypixelGuildMember(member))
+  }
+}
+
+async function fetchHypixel(endpoint: string, parameters: {[key: string]: string}): Promise<any> {
+  let url = new URL("https://api.hypixel.net")
+  url.pathname = endpoint
+  url.search = (new URLSearchParams(parameters)).toString()
+  const response = await fetchWithTimeout(url)
+  if (response.status == 200) {
+    return response.json()
+  } else {
+    throw new Error(`Hypixel API returned status ${response.status} ${response.statusText}`)
+  }
+}
--- command/commands/TrophyFishCommand.ts
+++ command/commands/TrophyFishCommand.ts
@@ -1,70 +1,62 @@
 import { Command } from "./Command.js"
-import { titleCase } from "../../utils/Utils.js"
-import { fetchProfiles } from "../../utils/apiUtils.js"
+import { apiKey, titleCase } from "../../utils/Utils.js"
 import { fetchUuid } from "../../utils/playerUtils.js"
-import { resolveProfile } from "../../utils/profileUtils.js"
+import { HypixelAPI } from "../../api/HypixelAPI.js"
+import { trophyFishNames } from "../../api/TrophyFish.js"
 
 export class TrophyFishCommand implements Command {
     aliases = ["trophy", "trophyfish", "tfish"]
     usage = "<player:[profile|bingo|main]> <total | tiers | [fish]>"
 
-    async execute(args: string[]) {
-        if (args.length < 2) return `Syntax: trophy ${this.usage}`
-        const playerArg = args.shift()!.split(":")
-        const playerName = playerArg[0]
-        const profileArg = playerArg[1]?.toLowerCase()
-        const arg = args.shift()!
-        let fish = args?.join("_")
-        if (arg !== "total" && arg !== "tiers") fish = `${arg}_` + fish
-        let message
-        try {
-            const uuid = await fetchUuid(playerName)
-            const profiles = await fetchProfiles(uuid)
-            const profile = resolveProfile(profileArg, uuid, profiles)
-            const cuteName = profile.cute_name
-            const data = profile?.members?.[uuid]?.['trophy_fish']
-            if (!data) {
-                return `No trophy fish data found for ${playerName}.`
-            }
-            message = `${titleCase(arg)} data for ${playerName} (${cuteName}): `
-            const obj = Object.entries(data) as [string, number][]
-            switch (arg) {
-                case "total":
-                    message += `Total trophy fish caught: ${data.total_caught}`
-                    break
-                case "tiers":
-                    const bronzeTotal = obj.filter(([key]) => key.includes("_bronze"))
-                    const bronzeUnlocked = bronzeTotal.filter(([, value]) => value > 0).length
-                    const silverTotal = obj.filter(([key]) => key.includes("_silver"))
-                    const silverUnlocked = silverTotal.filter(([, value]) => value > 0).length
-                    const goldTotal = obj.filter(([key]) => key.includes("_gold"))
-                    const goldUnlocked = goldTotal.filter(([, value]) => value > 0).length
-                    const diamondTotal = obj.filter(([key]) => key.includes("_diamond"))
-                    const diamondUnlocked = diamondTotal.filter(([, value]) => value > 0).length
-                    message = `Trophy fish tiers unlocked for ${playerName} (${cuteName}): `
-                    message += `Bronze: ${bronzeUnlocked}/${bronzeTotal.length} | Silver: ${silverUnlocked}/${silverTotal.length} | Gold: ${goldUnlocked}/${goldTotal.length} | Diamond: ${diamondUnlocked}/${diamondTotal.length}`
-                    break
-                default:
-                    const fsih = this.guessFish(obj, fish)
-                    if (!fsih) return "Invalid fish."
-                    const name = fsih[0]
-                    message = `${name} caught for ${playerName} (${cuteName}): `
-                    message += `Total ${titleCase(name.replaceAll("_", " "))}: ${fsih[1] ?? 0} | Bronze: ${data[`${name}_bronze`] ?? 0} | Silver: ${data[`${name}_silver`] ?? 0} | Gold: ${data[`${name}_gold`] ?? 0} | Diamond: ${data[`${name}_diamond`] ?? 0}`
-                    break
-            }
+    constructor(private hypixelAPI: HypixelAPI) {}
 
-        } catch (e) {
-            message = "Something went wrong, API might be down!"
-            console.error(e)
+    async execute(args: string[]) {
+      if (args.length < 2) return `Syntax: trophy ${this.usage}`
+      const playerArg = args.shift()!.split(":")
+      const playerName = playerArg[0]
+      const profileArg = playerArg[1]?.toLowerCase()
+      const arg = args.shift()!
+      let fish = args?.join(" ")
+      if (arg !== "total" && arg !== "tiers") fish = [arg, fish].join(" ")
+      let message
+      try {
+        const uuid = await fetchUuid(playerName)
+        const profiles = await this.hypixelAPI.fetchProfiles(uuid)
+        const profile = profiles.getByQuery(profileArg)
+        const cuteName = profile.cuteName
+        const trophyFish = profile.trophyFish
+        message = `${titleCase(arg)} data for ${playerName} (${cuteName}): `
+        switch (arg) {
+          case "total":
+            message += `Total trophy fish caught: ${trophyFish.total}`
+            break
+          case "tiers":
+            message = `Trophy fish tiers unlocked for ${playerName} (${cuteName}): `
+            message += `Bronze: ${trophyFish.unlocked("bronze")}/18 | Silver: ${trophyFish.unlocked("silver")}/18 | Gold: ${trophyFish.unlocked("gold")}/18 | Diamond: ${trophyFish.unlocked("diamond")}/18`
+            break
+          default:
+            const fishMatch = this.guessFish(fish)
+            if (!fishMatch) return "Invalid fish."
+            const name = fishMatch
+            const fishData = trophyFish.get(name)
+            message = `${name} caught for ${playerName} (${cuteName}): `
+            message += `Total ${titleCase(name)}: ${fishData.total} | Bronze: ${fishData.bronze} | Silver: ${fishData.silver} | Gold: ${fishData.gold} | Diamond: ${fishData.diamond}`
+            break
         }
-        return message
+      } catch (e) {
+        message = "Something went wrong, API might be down!"
+        console.error(e)
+      }
+      return message
     }
 
-    guessFish(data: [string, number][], input: string) {
-        let phrase = input.split("_")
-        let bestMatches = data.filter(([key, value]) => {
-            return phrase.some((phrase) => key.includes(phrase))
+    guessFish(input: string) {
+        let phrase = input.toLowerCase().trim().split(" ")
+        console.log(phrase)
+        let bestMatches = trophyFishNames.filter(name => {
+            return phrase.some((phrase) => name.toLowerCase().includes(phrase))
         })
+        console.log(bestMatches)
         let bestMatch
         if (bestMatches.length > 1) {
             bestMatch = bestMatches.sort((a, b) => {
@@ -76,3 +68,9 @@ export class TrophyFishCommand implements Command {
         return bestMatch
     }
 } 
+
+async function testTrophyFishCommand() {
+  const testAPI = new HypixelAPI(apiKey)
+  const command = new TrophyFishCommand(testAPI)
+  console.log(await command.execute(["appable:orange", "Sulphur"]))
+}
