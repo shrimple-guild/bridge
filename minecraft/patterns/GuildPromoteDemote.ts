a043207e4c29b1eaf50bcc9b8cf28245b7b4b4f7
IlmarsXd <ilmars500@gmail.com>
2023-04-08 21:57:32 +0300

    slight improvement

--- minecraft/patterns/GuildPromoteDemote.ts
+++ minecraft/patterns/GuildPromoteDemote.ts
@@ -1,11 +1,11 @@
 import { Pattern } from "./Pattern"
+import config from "../../config.json" assert { type: "json" }
 
 export const guildPromoteDemote: Pattern = {
   name: "guildPromoteDemote",
   pattern: /^\[(?<rank>[\w+]+)\] (?<name>\w{2,16}) was (?<action>promoted|demoted) from (?<initial>[\w ]+) to (?<final>[\w ]+)$/,
   execute: (bot, groups) => {
-    const promoted = groups.action == "promoted" 
-    const actionString = promoted ? "promoted" : "demoted"
-    bot.sendToBridge(groups.name, `${actionString} to ${groups.final}!`, promoted ? "JOINED" : "LEFT")
+    const promoted = groups.action == "promoted"
+    bot.sendToBridge(config.minecraft.username, `**${groups.name} was ${groups.action} to ${groups.final}!**`, promoted ? "JOINED" : "LEFT")
   }
 }
