b94c5b363072429321584a9bb62a48e39b6f76f3
IlmarsXd <ilmars500@gmail.com>
2023-08-24 00:21:15 +0300

    unused regex

--- src/utils/RegularExpressions.ts
+++ src/utils/RegularExpressions.ts
@@ -3,10 +3,7 @@ export const mcJoinLeavePattern = /^Guild > (?<username>\w{2,16}) (?<action>join
 export const limboRegex = /^You were spawned in Limbo.$/
 export const numberFormatRegex = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g
 export const imageLinkRegex = /https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp|bmp|tiff|svg)/g
-export const partyInviteRegex = /You have been invited to join (?<name>\w{2,16})'s party!/
-export const partyInviteRegex2 = /(?<name>\w{2,16}) has invited you to join their party!/
 export const spamRegex = /^You cannot say the same message twice!$/
-export const dungeonEnteredRegex = /^\[(?<rank>[\w+]+)\] (?<name>\w{2,16}) entered (?<dungeon>.+)$/m
 export const privateMessageRegex = /^From (?:\[(?<hypixelRank>[\w+]+)\] )?(?<name>\w{2,16}): (?<content>.+$)/
 export const guildLeaveRegex = /^\[(?<rank>[\w+]+)\] (?<name>\w{2,16}) left the guild!$/
 export const guildJoinRegex = /^\[(?<rank>[\w+]+)\] (?<name>\w{2,16}) joined the guild!$/
