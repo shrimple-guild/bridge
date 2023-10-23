b2f5511700d864762cd0f7c4c64351f9d94e26e9
IlmarsXd <ilmars500@gmail.com>
2023-10-23 22:06:22 +0300

    improve config getting and setting

--- src/minecraft/patterns/PrivateMessage.ts
+++ src/minecraft/patterns/PrivateMessage.ts
@@ -2,6 +2,7 @@ import { Pattern } from "./Pattern"
 import { config } from "../../utils/config.js"
 import { MinecraftBot } from "../MinecraftBot";
 import fs from "fs"
+import { sleep } from "../../utils/utils.js";
 
 export const privateMessage: Pattern = {
   name: "privateMessage",
@@ -25,9 +26,27 @@ async function changeConfig(bot: MinecraftBot, sender: string, path: string, new
   for (let i = 0; i < keys.length; i++) {
     if (i === keys.length - 1) {
       if (newProperty === undefined) {
-        await bot.chatRaw(`/msg ${sender} ${keys[i]}: ${current[keys[i]]} <@${Math.random().toString(36).substring(2)}>`)
+        const msgPrefix = `/msg ${sender} `
+        const random = ` <@${Math.random().toString(36).substring(2)}>`
+        const msg = `${keys[i]}: ${JSON.stringify(current[keys[i]])}`
+        if (msg.length < 256 - (msgPrefix.length + random.length)) await bot.chatRaw(msgPrefix + msg + random)
+        else {
+          let regex = new RegExp(`.{1,${200 - (msgPrefix.length + random.length)}}`, "g")
+          const split = msg.match(regex)
+          if (split) {
+            for (const chunk of split) {
+              await bot.chatRaw(msgPrefix + chunk + random)
+              await sleep(1000)
+            }
+          }
+        }
       } else {
-        current[keys[i]] = newProperty
+        let property = newProperty
+        try {
+          property = JSON.parse(newProperty)
+        } catch (e) {
+        }
+        current[keys[i]] = property
         await bot.chatRaw(`/msg ${sender} Changed ${keys[i]} to ${newProperty} <@${Math.random().toString(36).substring(2)}>`)
         fs.writeFile("./src/config.json", JSON.stringify(config, null, 2), (err) => {
           if (err) {
