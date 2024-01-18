28c3fd4049e057f4c9c92e6b40564e831b022cd0
Appability <appable@icloud.com>
2024-01-17 20:38:17 -0800

    rework bot (experimental)

---
+++ src/minecraft/IMineflayerBot.ts
@@ -0,0 +1,9 @@
+export interface IMineflayerBot {
+	on(event: "messagestr", callback: (chat: string) => void): void;
+	on(event: "end", callback: (reason: string) => void): void;
+	on(event: "spawn", callback: () => void): void;
+
+	chat(msg: string): void;
+	quit(): void;
+	isOnline(): boolean;
+}
--- src/minecraft/MinecraftBot.ts
+++ src/minecraft/MinecraftBot.ts
@@ -1,20 +1,15 @@
-import AsyncLock from "async-lock";
-import mineflayer from "mineflayer";
 import { Bridge } from "../bridge/Bridge.js";
-import { sleep } from "../utils/utils.js";
 import { PatternManager } from "./PatternManager.js";
 import { LoggerCategory } from "../utils/Logger.js";
 import { config } from "../utils/config.js";
 import pThrottle from "p-throttle";
+import { IMineflayerBot } from "./IMineflayerBot.js";
 
 export class MinecraftBot {
 	bridge?: Bridge;
 
-	private bot: mineflayer.Bot;
-	private status: "online" | "offline" = "offline";
-	private retries: number = 0;
+	private bot: IMineflayerBot;
 	private spamProtectionLastSent: number = 0;
-	private chatDelay = 1000;
 	private throttle = pThrottle({
 		strict: true,
 		interval: 1000,
@@ -22,33 +17,28 @@ export class MinecraftBot {
 	});
 
 	constructor(
+		bot: IMineflayerBot,
 		readonly username: string,
 		private privilegedUsers?: string[],
 		private logger?: LoggerCategory
 	) {
-		this.bot = this.connect(username);
+		this.bot = bot;
+
+		this.bot.on("spawn", () => {
+			void this.setOnline();
+		});
+
+		this.bot.on("messagestr", (message) => {
+			this.onChat(message);
+		});
+
+		this.bot.on("end", (reason) => {
+			void this.onEnd(reason);
+		});
 	}
 
 	getStatus() {
-		return this.status == "online";
-	}
-
-	connect(username: string) {
-		this.logger?.info("Connecting...");
-		const bot = mineflayer.createBot({
-			host: "mc.hypixel.net",
-			port: 25565,
-			username: username,
-			chatLengthLimit: 256,
-			auth: "microsoft",
-			version: "1.17.1",
-			checkTimeoutInterval: 10000
-		});
-		bot.on("messagestr", (chat) => this.onChat(chat));
-		bot.on("end", (reason) => this.onEnd(reason));
-		bot.once("spawn", () => this.onSpawn());
-		this.bot = bot;
-		return bot;
+		return this.bot.isOnline;
 	}
 
 	async sendToBridge(
@@ -76,9 +66,6 @@ export class MinecraftBot {
 	}
 
 	async setOnline() {
-		this.logger?.info("Bot online.");
-		this.status = "online";
-		this.retries = 0;
 		await this.bridge?.onBotJoin();
 	}
 
@@ -104,32 +91,7 @@ export class MinecraftBot {
 	}
 
 	async onEnd(reason: string) {
-		this.logger?.warn(`Disconnected (reason: ${reason}).`);
-		if (this.status == "online") await this.bridge?.onBotLeave(reason);
-		this.status = "offline";
-		if (reason != "disconnect.quitting") {
-			const waitTime = Math.min(
-				1000 * Math.pow(2, this.retries),
-				60 * 10 * 1000
-			);
-			await sleep(waitTime);
-			try {
-				this.connect(this.username);
-			} catch (e) {
-				await sleep(waitTime);
-				this.connect(this.username);
-				console.error(e);
-			}
-			this.retries++;
-			if (this.retries > 10) {
-				this.logger?.error("Failed to connect after 10 attempts. Quitting.");
-				process.exit(1);
-			}
-		}
-	}
-
-	async onSpawn() {
-		this.chat("§");
+		await this.bridge?.onBotLeave(reason);
 	}
 
 	isPrivileged(username: string) {
