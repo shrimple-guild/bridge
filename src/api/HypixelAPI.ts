334d5a1d63aa58ba4724c6d35cff2657306e9775
Appability <appable@icloud.com>
2024-01-19 14:28:27 -0800

    remove unused lock

--- src/api/HypixelAPI.ts
+++ src/api/HypixelAPI.ts
@@ -1,98 +1,113 @@
-import { Database } from "../database/database.js"
-import { LoggerCategory } from "../utils/Logger.js"
-import { PriorityLock } from "../utils/PriorityLock.js"
-import { Bazaar } from "./Bazaar.js"
-import { FarmingContests } from "./FarmingContests.js"
-import { HypixelAPIError } from "./HypixelAPIError.js"
-import { HypixelGuildMember } from "./HypixelGuildMember.js"
-import { HypixelPlayer } from "./HypixelPlayer.js"
-import { MojangAPI } from "./MojangAPI.js"
-import { SkyblockItems, SpecifiedNames } from "./SkyblockItems.js"
-import { SkyblockProfiles } from "./SkyblockProfiles.js"
+import { Database } from "../database/database.js";
+import { LoggerCategory } from "../utils/Logger.js";
+import { Bazaar } from "./Bazaar.js";
+import { FarmingContests } from "./FarmingContests.js";
+import { HypixelAPIError } from "./HypixelAPIError.js";
+import { HypixelGuildMember } from "./HypixelGuildMember.js";
+import { HypixelPlayer } from "./HypixelPlayer.js";
+import { MojangAPI } from "./MojangAPI.js";
+import { SkyblockItems, SpecifiedNames } from "./SkyblockItems.js";
+import { SkyblockProfiles } from "./SkyblockProfiles.js";
 
-const maxRequestTime = 3
+const maxRequestTime = 3;
 
 export class HypixelAPI {
+	readonly mojang: MojangAPI;
+	contests?: FarmingContests;
+	bazaar?: Bazaar;
 
-  readonly mojang: MojangAPI
-  contests?: FarmingContests
-  bazaar?: Bazaar
+	private rateLimit: number = 60;
+	private rateLimitRemaining: number = 60;
+	private rateLimitReset: number = 60;
 
-  private lock = new PriorityLock()
+	constructor(
+		private apiKey: string,
+		private database: Database,
+		private logger: LoggerCategory
+	) {
+		this.mojang = new MojangAPI(database);
+	}
 
-  private rateLimit: number = 60
-  private rateLimitRemaining: number = 60
-  private rateLimitReset: number = 60
+	async init(specifiedNames?: SpecifiedNames) {
+		const items = await SkyblockItems.create(this, specifiedNames);
+		this.bazaar = new Bazaar(this, items, this.logger);
+		this.contests = await FarmingContests.create(this.logger);
+	}
 
-  constructor(
-    private apiKey: string,
-    private database: Database,
-    private logger: LoggerCategory
-  ) {
-    this.mojang = new MojangAPI(database)
-  }
+	async fetchPlayer(uuid: string): Promise<HypixelPlayer> {
+		const { data } = await this.fetchHypixel("/player", {
+			uuid: uuid,
+			key: this.apiKey
+		});
+		if (data.player == null)
+			throw new Error(`This player has not joined Hypixel!`);
+		return new HypixelPlayer(data.player);
+	}
 
-  async init(specifiedNames?: SpecifiedNames) {
-    const items = await SkyblockItems.create(this, specifiedNames)
-    this.bazaar = new Bazaar(this, items, this.logger)
-    this.contests = await FarmingContests.create(this.logger)
-  }
+	async fetchProfiles(uuid: string): Promise<SkyblockProfiles> {
+		const uuidTrimmed = uuid.replaceAll("-", "");
+		const { data } = await this.fetchHypixel("/skyblock/profiles", {
+			uuid: uuidTrimmed,
+			key: this.apiKey
+		});
+		if (data.profiles == null || data.profiles.length == 0) {
+			throw new Error(`This player has not joined SkyBlock!`);
+		}
+		return new SkyblockProfiles(data.profiles, uuidTrimmed);
+	}
 
-  async fetchPlayer(uuid: string): Promise<HypixelPlayer> {
-    const { data } = await this.fetchHypixel("/player", { uuid: uuid, key: this.apiKey }) 
-    if (data.player == null) throw new Error(`This player has not joined Hypixel!`)
-    return new HypixelPlayer(data.player)
-  }
-  
-  async fetchProfiles(uuid: string): Promise<SkyblockProfiles> {
-    const uuidTrimmed = uuid.replaceAll("-", "")
-    const { data } = await this.fetchHypixel("/skyblock/profiles", { uuid: uuidTrimmed, key: this.apiKey }) 
-    if (data.profiles == null || data.profiles.length == 0) {
-      throw new Error(`This player has not joined SkyBlock!`)
-    }
-    return new SkyblockProfiles(data.profiles, uuidTrimmed)
-  }
-  
-  async fetchGuildMembers(guildId: string): Promise<HypixelGuildMember[]> {
-    const { data } = await this.fetchHypixel("/guild", { id: guildId, key: this.apiKey }) 
-    if (!data.guild) throw new Error(`This guild does not exist!`)
-    const members = data.guild.members as any[]
-    return members.map(member => new HypixelGuildMember(member, this))
-  }
+	async fetchGuildMembers(guildId: string): Promise<HypixelGuildMember[]> {
+		const { data } = await this.fetchHypixel("/guild", {
+			id: guildId,
+			key: this.apiKey
+		});
+		if (!data.guild) throw new Error(`This guild does not exist!`);
+		const members = data.guild.members as any[];
+		return members.map((member) => new HypixelGuildMember(member, this));
+	}
 
-  async fetchHypixel(endpoint: string, parameters: {[key: string]: string} = {}): Promise<HypixelResponse> {
-    let url = new URL("https://api.hypixel.net")
-    url.pathname = endpoint
-    url.search = (new URLSearchParams(parameters)).toString()
-    const response = await fetchWithTimeout(url)
-    if (response.status == 200) {
-      return {
-        data: await response.json(),
-        headers: {
-          rateLimit: parseInt(response.headers.get("ratelimit-limit") ?? "0"),
-          rateLimitRemaining: parseInt(response.headers.get("ratelimit-remaining") ?? "0"),
-          rateLimitReset: parseInt(response.headers.get("ratelimit-reset") ?? "0")
-        }
-      }
-    } else {
-      throw new HypixelAPIError(response.status, response.statusText)
-    }
-  }
+	async fetchHypixel(
+		endpoint: string,
+		parameters: { [key: string]: string } = {}
+	): Promise<HypixelResponse> {
+		let url = new URL("https://api.hypixel.net");
+		url.pathname = endpoint;
+		url.search = new URLSearchParams(parameters).toString();
+		const response = await fetchWithTimeout(url);
+		if (response.status == 200) {
+			return {
+				data: await response.json(),
+				headers: {
+					rateLimit: parseInt(response.headers.get("ratelimit-limit") ?? "0"),
+					rateLimitRemaining: parseInt(
+						response.headers.get("ratelimit-remaining") ?? "0"
+					),
+					rateLimitReset: parseInt(
+						response.headers.get("ratelimit-reset") ?? "0"
+					)
+				}
+			};
+		} else {
+			throw new HypixelAPIError(response.status, response.statusText);
+		}
+	}
 }
 
 export async function fetchWithTimeout(url: URL) {
-  try {
-    return await fetch(url, { signal: AbortSignal.timeout(maxRequestTime * 1000) })
-  } catch (e) {
-    throw new Error(`Request timed out.`)
-  }
+	try {
+		return await fetch(url, {
+			signal: AbortSignal.timeout(maxRequestTime * 1000)
+		});
+	} catch (e) {
+		throw new Error(`Request timed out.`);
+	}
 }
 
 type HypixelResponse = {
-  data: any,
-  headers: {
-    rateLimit: number,
-    rateLimitRemaining: number,
-    rateLimitReset: number
-  }
-}
+	data: any;
+	headers: {
+		rateLimit: number;
+		rateLimitRemaining: number;
+		rateLimitReset: number;
+	};
+};
