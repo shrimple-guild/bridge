039b3092cf6fdae25d0677152383a3aeeee1de22
IlmarsXd <ilmars500@gmail.com>
2024-08-02 01:23:01 +0300

    remove help, add boop command

--- src/bridge/commands/bridgeCommands/BoopDemonCommand.ts
+++ src/bridge/commands/bridgeCommands/BoopDemonCommand.ts
@@ -2,14 +2,22 @@ import { Bridge } from "../../Bridge.js"
 import { SimpleCommand } from "./Command.js"
 
 export class BoopDemonCommand implements SimpleCommand {
-  aliases = ["boop", "boopdemon", "bd"]
-  usage = "boop!"
+  aliases = ["boop"]
+  lastBoop = 0
 
   constructor(private bridge?: Bridge) {}
 
   async execute(args: string[]) {
-    this.bridge?.chatAsBot("/boop demonhunter990")
-    return "Booped!"
+    if (Date.now() - this.lastBoop < 60000 * 5) return "You're booping too fast! Wait a while before trying again."
+    if (args.length > 1) return "Too many arguments!"
+    const boop = args[0]
+    if (!boop) {
+      this.bridge?.chatAsBot("/boop demonhunter990")
+    } else {
+      this.bridge?.chatAsBot(`/boop ${boop}`)
+    }
+    this.lastBoop = Date.now()
+    return `Booped ${boop ?? "demonhunter990"}!`
   }
 } 
 
