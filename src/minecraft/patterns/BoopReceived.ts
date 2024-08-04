aa6fc3539171e1cca8750b08a293197178472960
IlmarsXd <ilmars500@gmail.com>
2024-08-04 18:16:55 +0300

    check exact boop

--- src/minecraft/patterns/BoopReceived.ts
+++ src/minecraft/patterns/BoopReceived.ts
@@ -3,7 +3,7 @@ import { Pattern } from "./Pattern"
 
 export const boopReceived: Pattern = {
   name: "boopReceived",
-  pattern: /^From (?:\[(?<hypixelRank>[\w+]+)\] )?(?<name>\w{2,16}): Boop!/,
+  pattern: /^From (?:\[(?<hypixelRank>[\w+]+)\] )?(?<name>\w{2,16}): Boop!$/,
   execute: async (bot, groups) => {
     const content = `Thanks for the boop, ${groups.name}! ${antiSpamProtString()}`
     bot.chat(content)
