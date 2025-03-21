ffce775319a6e40d483727acfd0b695224dcb88c
appable <16139460+appable0@users.noreply.github.com>
2025-03-20 18:54:29 -0700

    fix election command with specials

--- src/tests/electionTest.ts
+++ src/tests/electionTest.ts
@@ -1,15 +1,20 @@
 import ElectionCommandHelper from "../bridge/commands/bridgeCommands/ElectionCommandHelper.js"
 import election from "./data/election-standard.json" assert { type: "json" }
 import electionFoxy from "./data/election-foxy.json" assert { type: "json" }
+import electionSpecial from "./data/election-special.json" assert { type: "json" }
 
 const response = ElectionCommandHelper.getMayorSummary(election)
 console.log("Sample mayor response:")
 console.log(response)
 
-const specialResponse = ElectionCommandHelper.getSpecial("derp")
+const querySpecialResponse = ElectionCommandHelper.getSpecial("derp")
 console.log('Sample "Derpy" query response:')
-console.log(specialResponse)
+console.log(querySpecialResponse)
 
 const foxyResponse = ElectionCommandHelper.getMayorSummary(electionFoxy)
 console.log("Sample mayor response with foxy:")
 console.log(foxyResponse)
+
+const specialResponse = ElectionCommandHelper.getMayorSummary(electionSpecial)
+console.log("Sample mayor response with special mayor elected:")
+console.log(specialResponse)
