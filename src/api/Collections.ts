7184edc8ac6787efd770f4dc0da28efcde3f6d84
IlmarsXd <ilmars500@gmail.com>
2025-08-20 03:23:01 +0300

    check tiers from all members

--- src/api/Collections.ts
+++ src/api/Collections.ts
@@ -4,23 +4,17 @@ import { HypixelAPI } from "./HypixelAPI.js"
 
 export class Collections {
 	private collections: Collection[] = []
-	private tierRegex = /^(.*?)(-?\d+)$/
-	constructor(member: any, api: HypixelAPI) {
+	constructor(member: any, api: HypixelAPI, allCollectionTiers: UnlockedTierEntry) {
 		const unlockedTiers = member.player_data?.unlocked_coll_tiers as string[]
 		if (!unlockedTiers) return
-		const unlockedTiersParsed: UnlockedTierEntry = this.getMaxUnlockedTiers(unlockedTiers)
 
 		const collections = member.collection as { [key: string]: number }
 		if (!collections) return
 		api.collections!.forEach((category) => {
 			category.items.forEach((item) => {
-				console.log("Item ID: ", item.id)
-				const maxTier = unlockedTiersParsed[item.id] ?? 0
-				console.log("Max tier:", maxTier)
+				const maxTier = allCollectionTiers[item.id] ?? 0
 				const isMaxed = maxTier === item.maxTier
-				console.log("Is maxed:", isMaxed)
 				const amount = collections[item.id] ?? 0
-				console.log("Amount:", amount)
 				const formattedAmt = formatNumber(amount, 0, false)
 				const amtString = isMaxed
 					? formattedAmt
@@ -37,20 +31,6 @@ export class Collections {
 				})
 			})
 		})
-	}
-
-	getMaxUnlockedTiers(unlockedTiers: string[]): UnlockedTierEntry {
-		const maxUnlockedTiers: UnlockedTierEntry = {}
-		unlockedTiers.forEach((tierStr) => {
-			const match = tierStr.match(this.tierRegex)!
-			const collection = match[1].slice(0, match[1].length - 1).trim()
-			const tier = parseInt(match[2])
-			if (tier === -1) return
-			if (!maxUnlockedTiers[collection] || maxUnlockedTiers[collection] < tier) {
-				maxUnlockedTiers[collection] = tier
-			}
-		})
-		return maxUnlockedTiers
 	}
 
 	getByCategory(category: string) {
