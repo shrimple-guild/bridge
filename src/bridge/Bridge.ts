b5b8654a07fd5aa99bb124a13fbc6d3f952e473c
IlmarsXd <ilmars500@gmail.com>
2025-08-23 21:16:01 +0300

    this is getting annoying

--- src/bridge/Bridge.ts
+++ src/bridge/Bridge.ts
@@ -19,6 +19,9 @@ export class Bridge {
 	}
 
 	getDiscordChannelId(source: MessageSource) {
+		if (source === MessageSource.Raw) {
+			source = this.minecraft.lastSource ?? source
+		}
 		return source === MessageSource.Staff ? this.officerDiscordChannelId : this.discordChannelId
 	}
 
